adf394411ca56e524676f8570d60b9f1340bb100
Elvan Hidayat <elvanhd@gmail.com>
2023-03-16 15:28:50 +0700

    route validation

--- pages/users/_id.vue
+++ pages/users/_id.vue
@@ -1,3 +1,11 @@
 <template>
   <h1>A single user, with ID: {{ $route.params.id }}</h1>
 </template>
+
+<script>
+export default {
+  validate(data) {
+    return /^\d+$/.test(data.params.id);
+  },
+};
+</script>
