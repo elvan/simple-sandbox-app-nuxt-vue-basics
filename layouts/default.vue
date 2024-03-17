1b318178ce2b9edf36a7c0c32d723a8a255bf334
Elvan Hidayat <elvanhd@gmail.com>
2024-03-17 13:23:27 +0700

    Add project configuration and structure
    - Add .editorconfig for consistent coding styles
    - Include .gitignore for common Node.js and Nuxt artifacts
    - Create asset, component, layout, middleware, page, plugin, static, and store directories with READMEs
    - Introduce default Vue components for Logo and page layout
    - Configure nuxt.config.js for project setup
    - Setup package.json for project dependencies and scripts
    - Ensure coding style consistency across HTML, CSS, and JS files

---
+++ layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  -moz-osx-font-smoothing: grayscale;
+  -ms-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-size-adjust: 100%;
+  box-sizing: border-box;
+  font-size: 16px;
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  word-spacing: 1px;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+.button--green {
+  display: inline-block;
+  padding: 10px 30px;
+  border: 1px solid #3b8070;
+  border-radius: 4px;
+  color: #3b8070;
+  text-decoration: none;
+}
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+.button--grey {
+  display: inline-block;
+  margin-left: 15px;
+  padding: 10px 30px;
+  border: 1px solid #35495e;
+  border-radius: 4px;
+  color: #35495e;
+  text-decoration: none;
+}
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <section class="container">
+    <div>
+      <logo />
+      <h1 class="title">simple-sandbox</h1>
+      <h2 class="subtitle">Nuxt.js Sandbox Application</h2>
+      <div class="links">
+        <a href="https://nuxtjs.org/" target="_blank" class="button--green"
+          >Documentation</a
+        >
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+          >GitHub</a
+        >
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  text-align: center;
+}
+.title {
+  display: block;
+  color: #35495e;
+  font-weight: 300;
+  font-size: 100px;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  letter-spacing: 1px;
+}
+.subtitle {
+  padding-bottom: 15px;
+  color: #526488;
+  font-weight: 300;
+  font-size: 42px;
+  word-spacing: 5px;
+}
+.links {
+  padding-top: 15px;
+}
+</style>
